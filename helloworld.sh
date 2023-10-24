70d352042f83b3d73881b6c429571636084e787b
Btaylor007 <brekelseytaylor@gmail.com>
2023-10-24 16:04:31 -0700

    helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script Name:                  Hello World 
+# Author:                       Breanna Taylor
+# Date of latest revision:      10/24/2023
+# Purpose:                      Ops challenge 01
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main 
+
+echo "Hello world"
+
+
+# End
